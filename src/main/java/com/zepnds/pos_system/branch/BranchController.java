5fc5a8c964cbc8fd6216c14b0c59dc5f176fe7c5
zepnds <zepnds@gmail.com>
2024-09-18 22:26:33 +0800

    new updates for update branch in business

--- src/main/java/com/zepnds/pos_system/branch/BranchController.java
+++ src/main/java/com/zepnds/pos_system/branch/BranchController.java
@@ -1,7 +1,7 @@
 package com.zepnds.pos_system.branch;
 
 
-import com.zepnds.pos_system.merchant.MerchantResponse;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
